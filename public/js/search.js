33ba0cb4057e40e715793de2cab732136ebc1618
Ryan Peterson <petersonryan97@gmail.com>
2018-02-18 00:20:44 -0800

    searching

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,31 @@
+$(document).ready(function(){
+    $("#trail-search").keyup(function(event) {
+        if (event.keyCode === 13) {
+            search($(this).val());
+        }
+    });
+});
+
+
+var options = {
+    shouldSort: true,
+    threshold: 0.2,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+        "name",
+        "region"
+    ]
+};
+
 
 function search(string){
-
+    var fuse = new Fuse(trails, options);
+    var results = fuse.search(string);
+    if (!string || string == "" || string == " "){
+        results = trails;
+    }
+    drawCards(results);
 }
