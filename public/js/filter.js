a73d176aa6ed4d05517d0f50d278d5f609092122
Yagnik Vadher <yagniktechmail@gmail.com>
2018-02-18 02:30:42 -0800

    Current change

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,3 +1,5 @@
+window.nearBy = [];
+
 function filterDistance(minDistance, maxDistance){
     var results=[];
     $.each (trails, function(index, object){
@@ -6,4 +8,22 @@ function filterDistance(minDistance, maxDistance){
         }
     });
     drawCards(results);
+}
+
+function filterNearBy(range){
+    for (let i=0; i<trails.length ; i++){
+        let checkPoint = { 'lat': trails[i].lat, 'long': trails[i].long};
+        if (arePointsNear(checkPoint, window.pos, range)){
+            nearBy.push(trails[i]);
+        }
+    }
+}
+
+//helper functions
+function arePointsNear(checkPoint, centerPoint, km) {
+    var ky = 40000 / 360;
+    var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
+    var dx = Math.abs(centerPoint.long - checkPoint.long) * kx;
+    var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
+    return Math.sqrt(dx * dx + dy * dy) <= km;
 }
